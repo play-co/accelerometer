9ebe0d361dca5a6c01bf8841d01573bcf464249e
Joe Brown <sparkyjoe94@aim.com>
2015-01-26 01:05:33 -0800

    wrapped NATIVE.events.registerHandler
    with if statement to check for hasNativeEvents

--- js/accelerometer/index.js
+++ js/accelerometer/index.js
@@ -2,14 +2,16 @@ var hasNativeEvents = NATIVE && NATIVE.plugins && NATIVE.plugins.sendEvent;
 
 var handleAccel = null;
 
-NATIVE.events.registerHandler('accelerometerEvent', function(evt) {
-	if (handleAccel) {
-		// evt.x, evt.y, evt.z are unscaled x,y,z acceleration including gravity
-		// iOS seems to send 1.0 = force of gravity, but other devices may
-		// do it differently
-		handleAccel(evt);
-	}
-});
+if (hasNativeEvents) {
+	NATIVE.events.registerHandler('accelerometerEvent', function(evt) {
+		if (handleAccel) {
+			// evt.x, evt.y, evt.z are unscaled x,y,z acceleration including gravity
+			// iOS seems to send 1.0 = force of gravity, but other devices may
+			// do it differently
+			handleAccel(evt);
+		}
+	});
+}
 
 var Accelerometer = Class(function () {
 	this.start = function(handler) {
